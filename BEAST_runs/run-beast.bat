9b53c13bf8de2340e2cc60cf5be9ed7af4470570
John <52638982+Tay-j@users.noreply.github.com>
2021-02-23 15:05:44 +1100

    Cleanup

--- BEAST_runs/run-beast.bat
+++
@@ -1,4 +0,0 @@
-D:
-@echo off
-set /p file="Enter File: "
-java -Xmx16g -jar Documents\John\University\Master\Project\Tools\BEASTv2.6.3\lib\launcher.jar -threads 4 Documents\GitHub\bioinf-masters\BEAST_run\%file%.xml 
