b9880b05952a1ac9f366d2dc4017246a05da90d7
John <52638982+Tay-j@users.noreply.github.com>
2021-11-16 10:22:01 +1100

    Project
    BEAST tree building

--- CSIRO/get_sequences.r
+++ CSIRO/get_sequences.r
@@ -7,13 +7,15 @@ setwd("~/GitHub/bioinf-masters/CSIRO")
 
 ### Australian Dataset
 res.aus <- entrez_search(db = "nucleotide", term = "'Influenza A virus'
-[porgn:__txid11320] AND hemagglutinin NOT 'partial' AND H7N* AND Australia",
+[porgn:__txid11320] AND hemagglutinin NOT partial AND H7N* AND Australia",
                          retmax = 9999)
 
 #esums.aus <- entrez_summary(db = "nucleotide", id = res.aus$ids)
 
 fasta.aus <- entrez_fetch(db = "nucleotide", id = res.aus$ids, rettype = "fasta")
 fasta.aus <- strsplit(fasta.aus, ">")
+
+summ.aus <- entrez_summary(db="nucleotide", id=res.aus$ids)
 
 n.aus <- length(res.aus$ids)
 seq.aus <- list()
@@ -22,17 +24,26 @@ for (i in 1:n.aus) {
 }
 
 seq.aus <- paste0(">",seq.aus[][])
+
+for (i in 1:length(summ.aus)) {
+  new.date.aus <- strptime(gsub("/","-",summ.aus[[i]][["createdate"]]),"%Y-%m-%d")
+  seq.aus[i] <- gsub(" ","_",seq.aus[i])
+  seq.aus[i] <- sub("\n",paste0("/",format(new.date.aus,"%d-%m-%Y"),"\n"),seq.aus[i])
+}
+
 write.table(seq.aus,file="australian_H7.fasta",quote=FALSE,sep = "",row.names = FALSE,col.names = FALSE)
 
 
 ### Global Dataset excluding Australia
 ret.max <- 300
 res.global <- entrez_search(db = "nucleotide", term = "'Influenza A virus'
-[porgn:__txid11320] AND hemagglutinin NOT 'partial' AND H7N* NOT Australia",
+[porgn:__txid11320] AND hemagglutinin NOT partial AND H7N* NOT Australia",
                             retmax = ret.max)
 
 fasta.global <- entrez_fetch(db = "nucleotide", id = res.global$ids[1:ret.max], rettype = "fasta")
 fasta.global <- strsplit(fasta.global, ">")
+
+summ.global <- entrez_summary(db="nucleotide", id=res.global$ids)
 
 n.global <- length(res.global$ids)
 seq.global <- list()
@@ -41,4 +52,12 @@ for (i in 1:n.global) {
 }
 
 seq.global <- paste0(">",seq.global[][])
+
+for (i in 1:length(summ.global)) {
+  new.date.global <- strptime(gsub("/","-",summ.global[[i]][["createdate"]]),"%Y-%m-%d")
+  seq.global[i] <- gsub(" ","_",seq.global[i])
+  seq.global[i] <- sub("\n",paste0("/",format(new.date.global,"%d-%m-%Y"),"\n"),seq.global[i])
+}
+
 write.table(seq.global,file="global_H7.fasta",quote=FALSE,sep = "",row.names = FALSE,col.names = FALSE)
+
